208d1e3658a25a465f95fca6e3af915c5b0a751f
MatchaOnMuffins <henryzhangjcle@gmail.com>
2022-07-07 11:20:09 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express'); // using express
+const socketIO = require('socket.io');
+const http = require('http')
+const port = process.env.PORT || 8080 // setting the port
+let app = express();
+let server = http.createServer(app);
+app.use(express.static('public')) // using the public folder
+let io = socketIO(server, {serveClient: false}); // using socket.io
+
+// make a connection with the user from server side
+io.on('connection', (socket)=>{
+    console.log('New user connected');
+    //emit message from server to user
+
+    // on createMessage event from user
+    socket.on('createMessage', (message)=>{
+        console.log('createMessage', message);
+        // emit message from user to server
+    })
+
+    // when server disconnects from user
+    socket.on('disconnect', ()=>{
+        console.log('disconnected from user');
+    });
+});
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/assets/html/index.html')
+})
+
+
+server.listen(port,()=>{
+    console.log(`Server is running on port ${port}`)
+})
---
+++ public/js/index.js
@@ -0,0 +1,30 @@
+let socket = io()
+
+// connection with server
+socket.on('connect', function () {
+    console.log('connected to server')
+});
+
+// when disconnected from server
+socket.on('disconnect', function () {
+    console.log('Disconnected from Server')
+});
+
+function onclicks() {
+    // get the value of the input fields
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let message = document.getElementById('message').value;
+    // initialize all the values to empty
+    document.getElementById('name').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('message').value = '';
+
+    // send the message to the server
+    socket.emit('createMessage', {
+        name: name,
+        email: email,
+        message: message
+    })
+    return false;
+}
